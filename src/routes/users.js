3a94e24c003e977a86c9e3e4b2de2b2c11f08714
usama455 <muhammad.usama@surveyauto.com>
2021-10-02 12:06:47 +0500

    logger implemented

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,10 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
+import { logger } from "./../utils";
 
-router.get("/", (req,res)=>{
-    res.send("hello user")
-})
+router.get("/", (req, res) => {
+	logger.error("Test info log");
+	res.send("hello user");
+});
 
 module.exports = router;
