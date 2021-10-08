3224578027ad14515d06346a6b2a51440d0cb366
usama455 <muhammad.usama@surveyauto.com>
2021-10-08 23:56:37 +0500

    token based auth implemented

--- src/configure/passport.js
+++ src/configure/passport.js
@@ -3,6 +3,10 @@ import { passport } from "passport";
 import LocalStrategy from "passport-local";
 import User from "../services/users/model";
 import jwt from "jsonwebtoken";
+
+const passportJWT = require("passport-jwt");
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 // const Users = model("Users");
 
 export const initializePassport = async (passport) => {
@@ -25,6 +29,22 @@ export const initializePassport = async (passport) => {
 				usernameField: "email"
 			},
 			authenticateUser
+		)
+	);
+
+	passport.use(
+		new JWTStrategy(
+			{
+				jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+				secretOrKey: process.env.SECRET_KEY
+			},
+			async (jwtPayload, done) => {
+				//find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
+				const user = await User.findById(jwtPayload._id);
+				console.log(user);
+				if (!user) return done(null, false);
+				return done(null, user);
+			}
 		)
 	);
 
--- src/services/users/index.js
+++ src/services/users/index.js
@@ -3,7 +3,10 @@ import { register, getAll, login } from "./controller";
 import { checkUserExists } from "./helper";
 const router = new Router();
 
+const passport = require("passport");
+
 router.post("/register", checkUserExists, register);
-router.get("/getAll", getAll);
+//the client will send the JWT token in Authorization Header as a Bearer Token.
+router.get("/getAll", passport.authenticate("jwt", { session: false }), getAll);
 router.post("/login", login);
 export default router;
